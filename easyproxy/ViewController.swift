f9ecab422460210c211fc6fde95651c9651826cb
eldooon <eldonchan91@gmail.com>
2018-04-23 17:55:04 -0400

    first commit

---
+++ easyproxy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  easyproxy
+//
+//  Created by Eldon Chan on 4/23/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
